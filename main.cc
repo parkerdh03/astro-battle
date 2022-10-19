2f93376a4829c0f4fd8b507bca1d777a66f37762
parkerdh03 <89439615+parkerdh03@users.noreply.github.com>
2022-10-18 20:51:44 -0700

    Updated main.cc

---
+++ main.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include "opponent.h"
+#include "player.h"
+#include "cpputils/graphics/image.h"
+using namespace std;
+int main() {
+
+
+graphics::Image game_screen(800, 600);
+bool DrawRectangle (int x, int y, int width, int height, int red, int green, int blue);
+game_screen.DrawRectangle (0, 0, 800, 600, 0, 0, 0);
+Opponent obj1;
+OpponentProjectile obj2;
+Player obj3;
+PlayerProjectile obj4;
+obj1.Draw(game_screen);
+obj2.Draw(game_screen);
+obj3.Draw(game_screen);
+obj4.Draw(game_screen);
+game_screen.ShowUntilClosed("background.bmp");
+}
